e57ef6624fdbfff2913ccd62b9182d9fc4650bd3
ljyTlm <970955947@qq.com>
2019-06-07 00:46:58 +0800

    1.主页做了一半 初步感觉 ui看着不错

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,7 +1,43 @@
+/**
+ * 获取所有的在线用户
+ */
 function getUserOnline() {
     $.post("../TalkingAction/getAllUserOnline.do",
         function(data){
-            alert(data)
-        }
-    );
+            setTimeout("getUserOnline()", 60000);
+        });
 }
+
+/**
+ * 制作聊天记录模板
+ * @param model
+ * @returns {string}
+ */
+function getTalkingModel(model){
+    var html = "<div id='talk_"+model.id+"' class='panel panel-success'>";
+            html += "<div class='panel-heading'>"+model.name+"</div>";
+            html += "<div class='panel-body'>"+model.text+"</div>";
+            html += "<div class='panel-footer'>时间:"+model.time+" id:"+model.ip+"</div>"
+        html += "</div>";
+    return html;
+}
+
+/**
+ * 发送信息
+ */
+function subText(){
+    var text = $("#editor").val();
+    if(cheek.cheekString(text)){return;}
+
+    $.post("../TalkingAction/sendText.do", {text: text}, function (data) {
+        if (data == "fail"){
+            alert("登录过期！请重现登录");
+            $(window).attr("location", "../index.html");
+        }
+        $("#talkList").append(getTalkingModel(data.parseJSON()));
+    });
+}
+
+$(function () {
+
+});
